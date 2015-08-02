4218b8ab759c4a10b35db741fd8d9d7bcb69cd67
baidu <baidu@baidudeMacBook-Pro.local>
2015-08-02 22:22:07 +0800

    Initial Commit

---
+++ git_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  git_test
+//
+//  Created by baidu on 15/8/2.
+//  Copyright (c) 2015年 baidu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
